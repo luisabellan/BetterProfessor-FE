9c98a7719c8f310434b617817c274bb55dfb4efc
Tashby <tashby2714@gmail.com>
2020-06-01 19:56:07 -0400

    build folder

---
+++ client/build/precache-manifest.6492936940a2b74fb9ca80737c6848f5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "955c3ce2c0ae5f058cf77d80f9e60cc5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f35f68626e28f818d1c9",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "168a1ef86afd203d8356",
+    "url": "/static/js/2.ce49c0d7.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/static/js/2.ce49c0d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f35f68626e28f818d1c9",
+    "url": "/static/js/main.751d3773.chunk.js"
+  },
+  {
+    "revision": "6f0234b4b87d585d06e8",
+    "url": "/static/js/runtime-main.cfae3202.js"
+  }
+]);
