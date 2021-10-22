8b710564a7100c90da4b780c18e524a1a8447f3d
Luis Abellan <luis.abellan.dev@gmail.com>
2021-10-22 19:14:01 +0200

    changed api address to my backend

--- src/utils_MosharrafMusa/axiosWithAuth.js
+++ src/utils_MosharrafMusa/axiosWithAuth.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export const axiosWithAuth = () => {
   const token = localStorage.getItem("token");
   return axios.create({
-    baseURL: "https://best-professor.herokuapp.com/",
+    baseURL: process.env.API,
     headers: {
       Authorization: token,
     },
